e0d5d6d65bcf237b7304a227b3fd50ddd83e2d8d
Michael Tharpe <mtharpe@hashicorp.com>
2021-12-10 21:02:35 -0500

    Update to all docker files

--- fedora/push_to_github.sh
+++ fedora/push_to_github.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in $(ls -d */ | cut -f1 -d'/'); do docker push ubuntu$i:base ghcr.io/mtharpe/ubuntu$i:base; done
+for i in $(ls -d */ | cut -f1 -d'/'); do docker push fedora$i:base ghcr.io/mtharpe/fedora$i:base; done
