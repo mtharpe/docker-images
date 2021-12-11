4d7d16cb95b5cc77f60087472ef1e942fde06062
Michael Tharpe <mtharpe@hashicorp.com>
2021-12-11 01:37:31 -0500

    Update to include pull script

---
+++ pull_all_from_github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker pull ghcr.io/mtharpe/fedora35:base
+docker pull ghcr.io/mtharpe/fedoratoolbox:base
+docker pull ghcr.io/mtharpe/ubuntu22.04:base
+docker pull ghcr.io/mtharpe/ubuntu18.04:base
+docker pull ghcr.io/mtharpe/ubuntu20.04:base
+docker pull ghcr.io/mtharpe/ubuntu21.04:base
+docker pull ghcr.io/mtharpe/fedora34:base
+docker pull ghcr.io/mtharpe/fedora33:base
