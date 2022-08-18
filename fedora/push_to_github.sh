414242b0101cbe8d9925a8287c3f1f1878a0d7b3
Michael Tharpe <mtharpe@hashicorp.com>
2022-08-18 11:35:28 -0400

    Mass updates for Fedora

--- fedora/push_to_github.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in $(ls -d */ | cut -f1 -d'/'); do docker push fedora$i:base ghcr.io/mtharpe/fedora$i:base; done
--- ubuntu/push_to_github.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in $(ls -d */ | cut -f1 -d'/'); do docker push ubuntu$i:base ghcr.io/mtharpe/ubuntu$i:base; done
